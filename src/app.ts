a42ff7e4b5ae6718f1165b64e162b911b6c8eec9
nurulamin092 <mnurulamin092@gmial.com>
2023-05-30 05:26:55 +0600

    eslint prettier husky lint staged

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
-import  express, { Application, Request, Response }  from "express";
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express();
+const app: Application = express()
 
 app.use(cors())
 
-
 //parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
 
-app.get('/',(req:Request,res:Response)=> {
-    res.send('hello')
-});
-
-export default app;
-
+export default app
